b9437052da4bb0c4bd9007aa89173acfb05d26ae
Viktor Stanchev <me@viktorstanchev.com>
2015-09-20 14:25:37 -0700

    fix test bug

--- tests/test_print.py
+++ tests/test_print.py
@@ -7,14 +7,14 @@ from pirate.print import Printer
 
 class TestPrint(unittest.TestCase):
 
-    def test_print_results(self):
+    def test_print_results_remote(self):
         class MockTable:
             add_row = MagicMock()
             align = {}
         mock = MockTable()
         printer = Printer(False)
         printer.print = MagicMock()
-        with patch('prettytable.PrettyTable', return_value=mock) as prettytable:
+        with patch('veryprettytable.VeryPrettyTable', return_value=mock) as prettytable:
             results = [{
                 'magnet': 'dn=name',
                 'seeds': 1,
@@ -26,7 +26,7 @@ class TestPrint(unittest.TestCase):
             prettytable.assert_called_once_with(['LINK', 'SEED', 'LEECH', 'RATIO', 'SIZE', '', 'UPLOAD', 'NAME'])
             mock.add_row.assert_has_calls([call([0, 1, 2, '0.5', '3.0', 'MiB', 'never', 'name'])])
 
-    def test_print_results(self):
+    def test_print_results_local(self):
         class MockTable:
             add_row = MagicMock()
             align = {}

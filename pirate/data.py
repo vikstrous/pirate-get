a7be2cd82316899644b11e30664eeebd065a9750
Viktor Stanchev <me@viktorstanchev.com>
2016-11-04 20:42:48 -0700

    set version to 0.2.10

--- pirate/data.py
+++ pirate/data.py
@@ -5,7 +5,7 @@ import pkgutil
 def get_resource(filename):
     return pkgutil.get_data(__package__, 'data/' + filename)
 
-version = '0.2.9'
+version = '0.2.10'
 
 categories = json.loads(get_resource('categories.json').decode())
 sorts = json.loads(get_resource('sorts.json').decode())

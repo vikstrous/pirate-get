344a764535b7bf930dce718e48fc01fb0d7f3c9c
Viktor Stanchev <me@viktorstanchev.com>
2018-06-16 10:26:59 -0700

    bump version to 0.3.1

--- pirate/data.py
+++ pirate/data.py
@@ -5,7 +5,7 @@ import pkgutil
 def get_resource(filename):
     return pkgutil.get_data(__package__, 'data/' + filename)
 
-version = '0.3.0'
+version = '0.3.1'
 
 categories = json.loads(get_resource('categories.json').decode())
 sorts = json.loads(get_resource('sorts.json').decode())

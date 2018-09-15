3074abf463a24e750f02bcf33507aabb9361a22b
Augusto Morais <aflavio@gmail.com>
2018-09-14 22:11:29 -0300

    give more detail when there is not possible download a torrent file

--- pirate/torrent.py
+++ pirate/torrent.py
@@ -182,7 +182,8 @@ def save_torrents(printer, chosen_links, results, folder):
         try:
             torrent = get_torrent(info_hash)
         except urllib.error.HTTPError:
-            printer.print('There is no cached file for this torrent :(',
+            printer.print('There is no cached file for this torrent :('
+                          ' \nCode: {} - {}'.format(e.code, e.reason),
                           color='ERROR')
         else:
             open(file, 'wb').write(torrent)

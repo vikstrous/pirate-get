c0ca18f2daeebea7899c4f4f413e9f8727d23363
rnhmjoj <rnhmjoj@inventati.org>
2021-12-26 12:15:40 +0100

    set version to 0.4.1

--- pirate/data.py
+++ pirate/data.py
@@ -6,7 +6,7 @@ def get_resource(filename):
     return pkgutil.get_data(__package__, 'data/' + filename)
 
 
-version = '0.4.0'
+version = '0.4.1'
 
 categories = json.loads(get_resource('categories.json').decode())
 sorts = json.loads(get_resource('sorts.json').decode())
